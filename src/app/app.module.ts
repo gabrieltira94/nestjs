a4c0c89c178d72d3c28b1496692d64baf294bfdf
Gabriel <b194gab@gmail.com>
2023-06-04 15:19:13 +0300

    Added Provider Registration topic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MusclesController } from '@/muscles/muscles.controller';
+import { MuscleService } from '@/muscles/muscles.service';
 
 @Module({
   imports: [],
   controllers: [AppController, MusclesController],
-  providers: [AppService],
+  providers: [AppService, MuscleService],
 })
 export class AppModule { }
