5508422cff577d33aedd1b1a0c81beeb8a9075a1
Gabriel <b194gab@gmail.com>
2023-06-04 16:13:26 +0300

    Added Global middlleware setting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerMiddleware } from '@/common/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
       whitelist: true
     }),
   );
+
+  // To use Logger middleware as Global
+  // app.use(LoggerMiddleware);
 
   await app.listen(3001);
 }
